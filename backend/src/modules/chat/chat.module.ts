1a95948a27c3a0ad261359cecf159fa2dadd4486
Yupara <parviz.yusupov03@mail.ru>
2025-06-19 09:16:03 +0500

    Create chat.module.ts

---
+++ backend/src/modules/chat/chat.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from './message.entity';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Message])],
+  providers: [ChatService],
+  controllers: [ChatController],
+  exports: [ChatService],
+})
+export class ChatModule {}
