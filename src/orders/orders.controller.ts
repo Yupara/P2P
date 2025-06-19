28a5e9bf5e8b92f34f6138b6c033f364303fc9c3
Yupara <parviz.yusupov03@mail.ru>
2025-06-19 18:15:28 +0500

    Update orders.controller.ts

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,12 +1,43 @@
 import { Controller, Get } from '@nestjs/common';
-import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private ordersService: OrdersService) {}
-
   @Get()
   findAll() {
-    return this.ordersService.findAll();
+    return [
+      {
+        id: 1,
+        username: "Savak",
+        price: "70,96",
+        amount: "304,7685 USDT",
+        limits: "500 – 500 000 RUB",
+        payment: "SBP, кольнениe",
+        orders: 40,
+        percent: 10,
+        status: "online",
+      },
+      {
+        id: 2,
+        username: "Ищу_Партнеров",
+        price: "71,03",
+        amount: "128,8821 USDT",
+        limits: "500 – 500 000 RUB",
+        payment: "Пиметы, Овореол",
+        orders: 15,
+        percent: 10,
+        status: "warning",
+      },
+      {
+        id: 3,
+        username: "ПроданоМейкеро",
+        price: "71,03",
+        amount: "999,9999 USDT",
+        limits: "500 – 500 000 RUB",
+        payment: "SBP",
+        orders: 11,
+        percent: 10,
+        status: "online",
+      },
+    ];
   }
 }
