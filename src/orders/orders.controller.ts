496e9f1bc8e367faaae486c46c00dbb6f15f40ff
Yupara <parviz.yusupov03@mail.ru>
2025-06-19 18:17:17 +0500

    Update orders.controller.ts

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private ordersService: OrdersService) {}
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+}
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('orders')
 export class OrdersController {
