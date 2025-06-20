2cea38659189eaeeaaa1f82e9339b0b03852282e
Yupara <parviz.yusupov03@mail.ru>
2025-06-20 11:40:15 +0500

    Create index.tsx

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: 32 }}>
+      <h1>Green P2P Frontend</h1>
+      <p>Поздравляем! Фронтенд успешно развернут.</p>
+      <p>Если вы видите эту страницу, Next.js работает правильно.</p>
+    </div>
+  );
+};
+
+export default HomePage;
