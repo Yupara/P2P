e96d84d6e2e16f24db9423942a3a52fa4816aaa8
Yupara <parviz.yusupov03@mail.ru>
2025-06-28 19:48:00 +0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>It works!</h1>
+      <p>This is a minimal React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
