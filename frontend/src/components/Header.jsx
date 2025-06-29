24f663ef5b7797d55acbfa702abaa39861f45293
Yupara <parviz.yusupov03@mail.ru>
2025-06-29 07:30:35 +0500

    Create Header.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '1rem', backgroundColor: '#111', color: '#fff' }}>
+      <h1>P2P Обменник</h1>
+    </header>
+  );
+};
+
+export default Header;
