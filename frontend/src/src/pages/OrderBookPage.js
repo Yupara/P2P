4f0bbf3c2635b5e196fe9f0513bbd1e53243199d
Yupara <parviz.yusupov03@mail.ru>
2025-07-01 02:40:15 +0500

    Create OrderBookPage.js

---
+++ frontend/src/src/pages/OrderBookPage.js
@@ -0,0 +1,11 @@
+// src/pages/OrderBookPage.jsx
+import React from 'react';
+
+export default function OrderBookPage() {
+  return (
+    <div>
+      <h2>Лента объявлений</h2>
+      {/* здесь будет OrdersList */}
+    </div>
+  );
+}
