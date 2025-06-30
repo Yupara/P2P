d5ce1b1173af6eb1e6bec64ef5ad6181d8c335da
Yupara <parviz.yusupov03@mail.ru>
2025-06-30 23:07:32 +0500

    Create NotFound.js

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <div style={{ padding: "2em", textAlign: "center" }}>
+    <h1>404</h1>
+    <p>Страница не найдена.</p>
+  </div>
+);
+
+export default NotFound;
