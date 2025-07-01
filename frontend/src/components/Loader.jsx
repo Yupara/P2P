d5202da21dba001b319d60dd30292f83e7e12db2
Yupara <parviz.yusupov03@mail.ru>
2025-07-01 20:50:41 +0500

    Create Loader.jsx

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+// src/components/Loader.jsx
+import React from 'react';
+import './Loader.css';
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader" />
+    </div>
+  );
+}
