94736542c1576eb29e25808e7f9d1b1afc0badc7
Yupara <parviz.yusupov03@mail.ru>
2025-07-01 23:19:05 +0500

    Create Disputes.jsx

---
+++ frontend/src/pages/Disputes.jsx
@@ -0,0 +1,13 @@
+// src/pages/Disputes.jsx
+import React from 'react';
+
+const Disputes = () => {
+  return (
+    <div>
+      <h2>Споры</h2>
+      <p>Здесь будет управление и проверка открытых споров между пользователями.</p>
+    </div>
+  );
+};
+
+export default Disputes;
