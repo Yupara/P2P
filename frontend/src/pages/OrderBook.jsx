b256bf8831b5f94c709f0d38fec123fc38e833d5
Yupara <parviz.yusupov03@mail.ru>
2025-07-01 23:15:39 +0500

    Create OrderBook.jsx

---
+++ frontend/src/pages/OrderBook.jsx
@@ -0,0 +1,13 @@
+// src/pages/OrderBook.jsx
+import React from 'react';
+
+const OrderBook = () => {
+  return (
+    <div>
+      <h2>Order Book</h2>
+      <p>Здесь будут отображаться объявления на покупку и продажу криптовалют.</p>
+    </div>
+  );
+};
+
+export default OrderBook;
