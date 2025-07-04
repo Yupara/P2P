2a1ec18892deac7209f1c383864ac991688412ce
Yupara <parviz.yusupov03@mail.ru>
2025-07-04 22:36:46 +0500

    Update OrderBookPage.jsx

--- frontend/src/pages/OrderBookPage.jsx
+++ frontend/src/pages/OrderBookPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import OrderCard from './OrderCard';
+import OrderCard from '../components/OrderCard';
 import OrderFilters from '../components/OrderFilters';
 import './OrderBookPage.css';
 
