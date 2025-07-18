4e25bc109d3eaef9fcf6565e46fd7835ec785daf
Yupara <parviz.yusupov03@mail.ru>
2025-07-18 17:34:01 +0500

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+
+  // Позволяет Railway корректно передавать порт через переменную окружения
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server started on port ${port}`);
 }
 bootstrap();
